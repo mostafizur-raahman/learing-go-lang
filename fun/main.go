8a8faca864625611c1bac14ae6837687dd24132a
mostafiz-zaag <mostafizur@zaagsys.com>
2026-02-14 14:03:00 +0600

    learn fundamentals

---
+++ fun/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+var a int
+var b int = 10
+var c, d int
+
+// m := 100 // this is only inside function
+// f, g := 10, "Hello"
+
+var (
+	i int
+	j string
+	k float64
+)
+
+func main() {
+	var ok string
+	fmt.Println(ok)
+}
