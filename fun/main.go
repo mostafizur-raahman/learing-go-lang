d04d437c6e61e26c9f1e506ce447ed31c21bf02c
mostafiz-zaag <mostafizur@zaagsys.com>
2026-02-15 21:29:49 +0600

    func

--- fun/main.go
+++ fun/main.go
@@ -10,7 +10,6 @@ var c, d int
 // f, g := 10, "Hello"
 
 var (
-	i int
 	j string
 	k float64
 )
@@ -18,4 +17,50 @@ var (
 func main() {
 	var ok string
 	fmt.Println(ok)
+
+	arr := [10]int{1, 2, 3, 4, 5}
+	for i := 0; i < len(arr); i++ {
+		// fmt.Println(arr[i])
+	}
+
+	subArry := arr[:3]
+	fmt.Println(subArry)
+
+	sli := make([]int, 5, 6)
+	for i := 0; i < 5; i++ {
+		sli[i] = i
+	}
+	sli = append(sli, 10)
+	sli = append(sli, 10)
+	sli = append(sli, 10)
+
+	// fmt.Println("val ", sli)
+	// fmt.Println("length ", len(sli))
+	// fmt.Println("capacity ", cap(sli))
+
+	// maps
+
+	pirates := make(map[int]string)
+
+	pirates[0] = "Hello world!"
+	pirates[2] = "Ok bye, see you tomorrow"
+
+	for k, v := range pirates {
+		if k == 1 {
+			fmt.Println("Print 1")
+			break
+		} else {
+			fmt.Println("Okkkkk", v)
+			break
+		}
+	}
+
+	prothom, ditio := returnTwo(1, 2)
+	fmt.Println(prothom, ditio)
+}
+
+func returnTwo(a, b int) (jogfol, biyoufol int) {
+	jogfol = a + b
+	biyoufol = a - b
+	return
 }
